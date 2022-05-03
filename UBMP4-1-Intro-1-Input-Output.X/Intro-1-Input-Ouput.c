0f8af80000978b92151ef056547a5ee80e81b05a
carlito.d.murphy@gmail.com <carlito.d.murphy@gmail.com>
2022-05-03 18:54:59 +0000

    new songs

--- UBMP4-1-Intro-1-Input-Output.X/Intro-1-Input-Ouput.c
+++ UBMP4-1-Intro-1-Input-Output.X/Intro-1-Input-Ouput.c
@@ -17,12 +17,22 @@ void playNote(int period, int durationCycles, bool isRest) {
      for(int j = 0; j < period; j++);
  }
 }
-const int C = 329;
-const int D = 293;
-const int E = 261;
-const int G = 391;
-const int q = 500;
-const int t = 2000;
+// const int C = 329;
+// const int D = 293;
+// const int E = 261;
+// const int G = 391;
+// const int q = 500;
+// const int t = 2000;
+
+        const int Ash = 466;
+        const int Dsh = 622;
+        const int C = 523;
+        const int D = 587;
+        const int G = 392;
+        const int Gsh = 415;
+        const int LDsh = 311;
+        const int q = 250;
+        const int t = 250;
 
 int main(void)
 {
@@ -32,38 +42,20 @@ int main(void)
      {
 if(SW5 == 0 && SW4==1 && SW3==1)
      {
-playNote(E, q, false);
-playNote(E, q, true);
+playNote(Ash, q, false);
+playNote(Ash, q, true);
 
-playNote(D, q, true);
-playNote(D, q, false);
+playNote(Dsh, q, true);
+playNote(Dsh, q, false);
+
+playNote(Ash, q, true);
+playNote(Ash, q, false);
 
 playNote(C, q, true);
 playNote(C, q, false);
 
 playNote(D, q, true);
 playNote(D, q, false);
-
-playNote(E, q, true);
-playNote(E, q, false);
-
-playNote(E, q, true);
-playNote(E, q, false);
-
-playNote(E, q, true);
-playNote(E, t, false);
-
-playNote(D, q, true);
-playNote(D, q, false);
-
-playNote(D, q, true);
-playNote(D, q, false);
-
-playNote(D, q, true);
-playNote(D, t, false);
-
-playNote(E, q, true);
-playNote(E, q, false);
 
 playNote(G, q, true);
 playNote(G, q, false);
@@ -71,44 +63,65 @@ playNote(G, q, false);
 playNote(G, q, true);
 playNote(G, t, false);
 
-playNote(E, q, true);
-playNote(E, q, false);
-
-playNote(D, q, true);
-playNote(D, q, false);
-
 playNote(C, q, true);
 playNote(C, q, false);
 
-playNote(D, q, true);
-playNote(D, q, false);
+playNote(Ash, q, true);
+playNote(Ash, q, false);
 
-playNote(E, q, true);
-playNote(E, q, false);
+playNote(Gsh, q, true);
+playNote(Gsh, t, false);
 
-playNote(E, q, true);
-playNote(E, q, false);
+playNote(Ash, q, true);
+playNote(Ash, q, false);
 
-playNote(E, q, true);
-playNote(E, q, false);
+playNote(LDsh, q, true);
+playNote(LDsh, q, false);
 
-playNote(E, q, true);
-playNote(E, q, false);
 
-playNote(D, q, true);
-playNote(D, q, false);
 
-playNote(D, q, true);
-playNote(D, q, false);
 
-playNote(E, q, true);
-playNote(E, q, false);
+            // playNote(G, q, true);
+            // playNote(G, t, false);
 
-playNote(D, q, true);
-playNote(D, q, false);
+            // playNote(E, q, true);
+            // playNote(E, q, false);
 
-playNote(C, q, true);
-playNote(C, t, false);
+            // playNote(D, q, true);
+            // playNote(D, q, false);
+
+            // playNote(C, q, true);
+            // playNote(C, q, false);
+
+            // playNote(D, q, true);
+            // playNote(D, q, false);
+
+            // playNote(E, q, true);
+            // playNote(E, q, false);
+
+            // playNote(E, q, true);
+            // playNote(E, q, false);
+
+            // playNote(E, q, true);
+            // playNote(E, q, false);
+
+            // playNote(E, q, true);
+            // playNote(E, q, false);
+
+            // playNote(D, q, true);
+            // playNote(D, q, false);
+
+            // playNote(D, q, true);
+            // playNote(D, q, false);
+
+            // playNote(E, q, true);
+            // playNote(E, q, false);
+
+            // playNote(D, q, true);
+            // playNote(D, q, false);
+
+            // playNote(C, q, true);
+            // playNote(C, t, false);
      }
      if(SW3 == 0)
      {
@@ -122,3 +135,42 @@ if(SW1 == 0)
      }
 }
 
+
+
+
+
+
+
+/*
+
+notes for song
+
+A sharp (b flat)    466
+D sharp (e flat)    622
+A sharp (b flat)    466
+C                   523
+D                   587
+G long              392
+G short             392
+C5                  523
+A sharp             466
+G sharp             415
+A sharp             466
+D sharp             311
+
+        const int Ash = 466;
+        const int Dsh = 622;
+        const int C = 523;
+        const int D = 587;
+        const int G = 392;
+        const int Gsh = 415;
+        const int LDsh = 311;
+
+        const int q = 500;
+        const int t = 2000;
+
+
+
+
+
+ */
